187b48e78bffdd46e03de268c0ab3c565b7f1120
luckynha <168048279+luckynha@users.noreply.github.com>
2025-07-22 14:25:59 +0000

    correção de variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -119,7 +119,7 @@ int main() {
     printf("*****Comparação de Cartas*****\n");
     printf("Considere que resultado (1) a carta 1 vence, e resultado (0) a carta 2 vence\n");
     printf("População: %d\n", comparacaoHabitantes);
-    printf("Área: %d\n", comparacaoArea);
+    printf("Área: %.2f\n", comparacaoArea);
     printf("PIB: %.2f\n", comparacaoPib);
     printf("Pontos Turísticos: %.2f\n", comparacaoPontosTuristicos);
     printf("Densidade Populacional: %.2f\n", comparacaoDensidadePopulacional);
